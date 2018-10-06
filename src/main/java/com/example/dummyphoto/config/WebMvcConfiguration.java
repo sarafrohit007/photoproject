453ac0fdec8cf0acd2e9b41716fb9062ae887105
sarafrohit007 <rhsaraf97@gmail.com>
2018-10-06 14:42:42 +0530

    Committing

--- src/main/java/com/example/dummyphoto/config/WebMvcConfiguration.java
+++ src/main/java/com/example/dummyphoto/config/WebMvcConfiguration.java
@@ -6,13 +6,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import com.example.dummyphoto.util.Constants;
+
 @Configuration
 @EnableWebSecurity
 public class WebMvcConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+	    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).maximumSessions(Constants.MAXIMUM_CONCURRENT_USER_CONNECTION_ALLOWED);
 	}
 	
 }
