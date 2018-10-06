3487050b914d03b76411726f49d48be470cab25b
sarafrohit007 <rhsaraf97@gmail.com>
2018-10-06 14:20:44 +0530

    Committing project

--- src/main/java/com/example/dummyphoto/config/WebMvcConfiguration.java
+++ src/main/java/com/example/dummyphoto/config/WebMvcConfiguration.java
@@ -1,5 +1,18 @@
 package com.example.dummyphoto.config;
 
-public class WebMvcConfiguration{
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
+@Configuration
+@EnableWebSecurity
+public class WebMvcConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+	}
+	
 }
