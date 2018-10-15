ef0fe5647be82e918cd2bd4ba4f6385059f25b0c
sarafrohit007 <rhsaraf97@gmail.com>
2018-10-16 03:48:39 +0530

    Committing changes

--- src/main/java/com/example/dummyphoto/DummyphotoApplication.java
+++ src/main/java/com/example/dummyphoto/DummyphotoApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DummyphotoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DummyphotoApplication.class, args);
 	}
-	
-	
 }
--- src/main/resources/templates/scripts/login.js
+++ src/main/resources/templates/scripts/login.js
@@ -1,17 +1,3 @@
     function onSignIn(googleUser){
-    	alert("Successfully signed in");
-    	var profile = googleUser.getBasicProfile();
-    	$(".g-signin2").css("display":"none");
-    	$(".data").css("display":"block");
-    	$("#pic").attr("source",profile.getImageUrl());
-    	$("#email").text(profile.getEmail());
-    }
-    
-    function signOut(){
-    	var auth2 =  gapi.auth2.getAuthInstance();
-    	auth.signOut.then(function(){
-    		alert("You have successfully signed out....");
-    		$(".g-signin2").css("display":"block");
-    		$(".data").css("display":"none");
-    	});
+    	alert("Successfully signed in");	
     }
