508f075b9bae2547c97f72f9ad641e489eea23ad
sarafrohit007 <rhsaraf97@gmail.com>
2018-11-02 06:56:30 +0530

    Committing Profile Service

---
+++ src/main/java/com/example/dummyphoto/profileService/IProfileService.java
@@ -0,0 +1,5 @@
+package com.example.dummyphoto.profileService;
+
+public interface IProfileService {
+
+}
---
+++ src/main/java/com/example/dummyphoto/profileService/impl/ProfileserviceImpl.java
@@ -0,0 +1,7 @@
+package com.example.dummyphoto.profileService.impl;
+
+import com.example.dummyphoto.profileService.IProfileService;
+
+public class ProfileserviceImpl implements IProfileService{
+
+}
